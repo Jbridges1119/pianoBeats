e8b3a086b7e9350fd2fed780ad8468331bf349c2
Jeff <jbridges1119@gmail.com>
2022-08-26 14:19:46 -0600

    Add Wrapper to keys

--- src/Octave.tsx
+++ src/Octave.tsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import {NoteType} from './helpers'
-import Note from './Note'
+import React from "react";
+import { NoteType } from "./helpers";
+import Note from "./Note";
+import { Wrapper } from "./styles/Octave.style";
 
 type Props = {
   //This was made in helpers - {[key:string]: string}
-  notes: NoteType[]
-}
+  notes: NoteType[];
+};
 
 //Declare the functional component and the Props type from above
-const Octave: React.FC<Props> = ({notes}) => {
+const Octave: React.FC<Props> = ({ notes }) => {
   const note = notes.map((element: NoteType) => (
     //Note desc - if color is white/black return White/Black(style) with note - black works opposite
-    <Note 
-    key={element.note}
-    color={element.color}
-    note={element.note}
-    />
-  ))
-
+    <Note key={element.note} color={element.color} note={element.note} />
+  ));
 
   return (
-    <div>
-      {note}
-    </div>
-  )
-}
+    <Wrapper>
+      <div>{note}</div>
+    </Wrapper>
+  );
+};
 
-export default Octave
+export default Octave;
