7d120efa4d126bd1d3a11e41e31aa92cd0009c0c
Jeff <jbridges1119@gmail.com>
2022-08-26 17:11:59 -0600

    Create Volume buttons

---
+++ src/components/Volume.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {VolumeKnob, Indicator} from '../styles/Volume.style'
+import { VolumeWrapper } from '../styles/Octave.style'
+
+
+type Props = {
+  raiseVolume: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  lowerVolume: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  volume: number;
+};
+
+const Volume: React.FC<Props> = ({raiseVolume, lowerVolume, volume}) => {
+  return (
+    <VolumeWrapper>
+    <VolumeKnob onClick={raiseVolume}>Volume Up</VolumeKnob>
+    <VolumeKnob onClick={lowerVolume}>Volume Down</VolumeKnob>
+    <Indicator>{Math.round(volume* 100)}%</Indicator>
+    </VolumeWrapper>
+  )
+}
+
+export default Volume
