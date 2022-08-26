b5f378405701d29911e1b574b14a05d381eef88d
Jeff <jbridges1119@gmail.com>
2022-08-26 17:11:46 -0600

    Create volume buttons and pass props

--- src/components/Octave.tsx
+++ src/components/Octave.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { NoteType } from "../helpers/helpers";
 import Note from "./Note";
 import { Wrapper } from "../styles/Octave.style";
-
+import Volume from './Volume';
 type Props = {
   //NoteType[] was made in helpers - {[key:string]: string}
   notes: NoteType[];
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  raiseVolume: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  lowerVolume: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  volume: number;
 };
 
 //Declare the functional component and the Props type from above
-const Octave: React.FC<Props> = ({ notes, onClick }) => {
+const Octave: React.FC<Props> = ({ notes, onClick, raiseVolume, lowerVolume, volume }) => {
   const note = notes.map((element: NoteType) => (
     //Note desc - if color is white/black return White/Black(style) with note - black works opposite
+    
     <Note
       key={element.note}
       color={element.color}
@@ -23,7 +27,11 @@ const Octave: React.FC<Props> = ({ notes, onClick }) => {
 
   return (
     <Wrapper>
-      <div>{note}</div>
+      <div>
+        {note}
+      <Volume raiseVolume={raiseVolume} lowerVolume={lowerVolume} volume={volume}/>
+      
+      </div>
     </Wrapper>
   );
 };
