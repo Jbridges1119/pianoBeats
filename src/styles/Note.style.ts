484d7b60dfdec0b0f8c08af4a1283751c8608592
Jeff <jbridges1119@gmail.com>
2022-08-26 14:18:58 -0600

    Add key styling

--- src/styles/Note.style.ts
+++ src/styles/Note.style.ts
@@ -2,7 +2,28 @@ import styled from 'styled-components'
 
 
 export const Black = styled.button`
-background: black;`
+  width: 40px;
+  height: 130px;
+  background: black;
+  border: solid black 1px;
+  position: absolute;
+  margin: 1px;
+  margin-left: -20px;
+  :active {
+    background: #333;
+  }
+`;
 
 export const White = styled.button`
-background:white;`
+  width: 60px;
+  height: 200px;
+  background: white;
+  border: solid black 1px;
+  box-shadow: 2px 5px;
+  margin: 1px;
+  margin-left: '-20px';
+  box-sizing: border-box;
+  :active {
+    background: #eee;
+  }
+`;
