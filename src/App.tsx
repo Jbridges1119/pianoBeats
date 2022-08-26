4d8df6bdef8dc93b13af3fbdac52902e037339d9
Jeff <jbridges1119@gmail.com>
2022-08-26 14:48:34 -0600

    Create onclick handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import {notes } from './helpers'
-import Octave from './Octave'
-
-
+import React, { MouseEvent } from "react";
+import { notes } from "./helpers/helpers";
+import Octave from "./components/Octave";
 
 function App() {
+  //e: MouseEvent<HTMLButtonElement> types the event - <HTMLButtonElement> makes it only work with button elements- nothing else
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    console.log(e.currentTarget.value);
+  };
   return (
     <div>
-     <Octave notes={notes}/>
+      <Octave notes={notes} onClick={handleClick} />
     </div>
   );
 }
